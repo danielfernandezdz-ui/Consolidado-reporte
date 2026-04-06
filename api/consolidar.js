2319e9c516c6d90f81abeab746a2459eb430f4fe
danielfernandezdz-ui <daniel.fernandezdz@gmail.com>
2026-04-06 14:22:08 -0400

    V2

--- api/consolidar.js
+++ api/consolidar.js
@@ -1,4 +1,4 @@
-export const config = { maxDuration: 60 };
+export const config = { maxDuration: 60, api: { bodyParser: { sizeLimit: '20mb' } } };
 
 export default async function handler(req, res) {
   res.setHeader('Access-Control-Allow-Origin', '*');
